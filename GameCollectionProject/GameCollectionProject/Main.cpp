14fafb0f5f21a38fbc0c24c8bee503debe72c94d
Clark <a1clark1a@gmail.com>
2018-01-22 17:19:48 -0800

    Update

--- GameCollectionProject/GameCollectionProject/Main.cpp
+++ GameCollectionProject/GameCollectionProject/Main.cpp
@@ -3,23 +3,12 @@
 ************************************************************************************************/
 #include "SpaceShooter.h"
 #include "MainMenu.h"
-//TODO Create a SoundManager Class to handle playing sounds  and background Music
 //TODO (In future) Create SpriteManager Class to handle sprite animations and sprite drawing functions to every sprite in each game class
-//TODO Create a Main Menu Class to handle creation of Game type based on player choice
 //TODO (In future) Particle Effect class
 
-
-//CURRENTLY BEING USED FOR DEBUGGING PURPOSE
 int main()
 {
 	
-	/*SpaceShooter shooter;
-	while (!shooter.GetWindow()->IsClosed())
-	{
-		shooter.Update(shooter.GetWindow()->GetDeltaTime()->asSeconds());
-		shooter.Render();
-	}
-	*/
 	MainMenu menu;
 	while (menu.IsProgramOn())
 	{
